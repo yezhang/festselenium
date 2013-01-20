44d441476f48f0f23e2e769f31fe11f9054fe12b
licanhua <licanhua@gmail.com>
2013-01-20 04:59:44 +0000

    rename from seleniumfest to festselenium

--- src/main/java/com/nemo/seleniumfest/fixture/MenuItemFixture.java
+++
@@ -1,8 +0,0 @@
-package com.nemo.seleniumfest.fixture;
-
-public class MenuItemFixture  extends ComponentFixture<MenuItemFixture>{
-
-	public MenuItemFixture(ComponentContext context) {
-		super(context);
-	}
-}
